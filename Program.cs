a2607b90f416dbd018966e9ccf567146bf3b4657
Sumaya-10 <sumayam-ab108@code.edu.az>
2024-10-18 01:39:55 +0400

    WEAPON

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _16oktclassroomtask;
+
+ public class Program
+{
+    static void Main(string[] args)
+    {
+        Weapon weapon = new Weapon(70, 40, 3.6f, "Auto");
+        int number = 0;
+        bool process = true;
+        while (process)
+        {
+            number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 0:
+
+                    Console.WriteLine("number verildi");
+                    break;
+
+                case 1:
+                    {
+                        weapon.Shoot();
+                        break;
+                    }
+
+                case 2:
+                    weapon.Fire();
+                    break;
+
+                case 3:
+                    Console.WriteLine(weapon.GetRemainBulletCount());
+                    break;
+                case 4:
+                    weapon.Reload();
+                    break;
+
+                case 5:
+                    weapon.ChangeMod();
+                    break;
+
+                case 6:
+                    Console.WriteLine("prases bitdi");
+                    process = false;
+                    break;
+
+                default:
+
+                    Console.WriteLine("Duzgun number daxil edilmedi");
+                break;
+                }
+            }
+        }
+    }
+ 
---
+++ Weapon.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+
+namespace _16oktclassroomtask;
+
+public class Weapon
+{
+    public int GunCapasity { get; set; }
+    public int BulletCount { get; set; }
+    public float GunDischarge { get; set; }
+    public string ShootMood { get; set; }
+    public Weapon()
+    {
+
+    }
+    public Weapon(int gunCapasity, int bulletCount, float gunDischarge, string shootMood)
+    {
+        if (gunCapasity < 0 || bulletCount < 0 || gunDischarge <= 0)
+            return;
+        GunCapasity = gunCapasity;
+        BulletCount = bulletCount;
+        GunDischarge = gunDischarge;
+        ShootMood = shootMood;
+    }
+
+    public void Shoot()
+    {
+        if (BulletCount > 0)
+        {
+            BulletCount--;
+            Console.WriteLine("new bullet count; " + BulletCount);
+        }
+        else
+        {
+            Console.WriteLine("Hal-hazirda daraqda gulle yoxdur");
+        }
+    }
+    public void Fire()
+    {
+        if (BulletCount > 0)
+        {
+            if (ShootMood == "Auto")
+            {
+                float spentTime = (BulletCount * GunDischarge) / GunCapasity;
+                Console.WriteLine($"{BulletCount} gulle istifade edildi. Ates {spentTime} saniye davam etdi");
+
+                BulletCount = 0;
+            }
+            else
+            {
+                Shoot();
+            }
+        }
+         else
+         {
+            Console.WriteLine("Hal-hazirda daraqda gulle yoxdur!!!");
+         }
+
+    }
+
+    public int GetRemainBulletCount()
+    {
+        int remainBulletsCount = GunCapasity - BulletCount;
+        return remainBulletsCount;
+    }
+
+    public void Reload()
+    {
+        if (BulletCount != GunCapasity)
+        {
+            Console.WriteLine("Daraq deyisdirildi");
+            BulletCount = GunCapasity;
+        }
+        else
+        {
+            Console.WriteLine("doludur!");
+        }
+    }
+
+    public void ChangeMod()
+    {
+        if (ShootMood == "Auto")
+        {
+            ShootMood = "Single";
+        }
+        else
+        {
+            ShootMood = "Auto";
+        }
+
+        Console.WriteLine("Atis modu" + ShootMood + "olaraq deyisdirildi");
+    }
+}
