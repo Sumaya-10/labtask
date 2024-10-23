4a78616c0a4cfcb1668d7333f587a6c6a29de08d
Sumaya-10 <sumayam-ab108@code.edu.az>
2024-10-23 22:15:23 +0400

    Add files via upload

---
+++ array/Program (1).cs
@@ -0,0 +1,15 @@
+﻿// matrisin elementlerini 2ye vur ekrana ver
+int[,] numbers =
+{
+    {2,4,6,8 },
+    {1,3,5,7 },
+    {1,2,3,5 },
+    {4,6,9,10 }
+};
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        Console.WriteLine(numbers[i, j]*2);
+    }
+}
